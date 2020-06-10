779c808e1549f4108807a5384dcbecafc77d295a
Hui Yu <ityuhui@gmail.com>
2020-06-10 09:23:48 +0800

    Create generate_client_c.sh

---
+++ generate_client_c.sh
@@ -0,0 +1,10 @@
+mvn clean package
+
+echo "begin to generate..."
+
+sleep 5
+
+java -jar modules/openapi-generator-cli/target/openapi-generator-cli.jar generate -i /root/c/kubernetes/swagger.json   -g c    -o ../kubernetes-client-c --skip-validate-spec
+
+
+echo "generated."
