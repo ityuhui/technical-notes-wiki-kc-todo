dc84a1e92a61f6781819ce89c1d76b5f209d3b96
Hui Yu <ityuhui@gmail.com>
2021-12-20 16:54:28 +0800

    Update and rename generate_client_c.sh to kubernetes-generate-c-client-from-yaml.sh

--- kubernetes-generate-c-client-from-yaml.sh
+++ kubernetes-generate-c-client-from-yaml.sh
@@ -5,6 +5,6 @@ echo "begin to generate..."
 sleep 5
 
 java -jar modules/openapi-generator-cli/target/openapi-generator-cli.jar generate -i /root/c/kubernetes/swagger.json   -g c    -o ../kubernetes-client-c --skip-validate-spec
-
+--type-mappings "int-or-string=IntOrString"
 
 echo "generated."
